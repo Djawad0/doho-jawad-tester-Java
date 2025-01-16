851a936d1a43f8a98ce5d2b9a8969f24973e2bf5
Djawad0 <doho.jawad.pro@gmail.com>
2025-01-16 15:21:15 +0100

    feat: Ajout des tests d’intégration

--- src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
@@ -42,18 +42,14 @@ public class ParkingServiceTest {
     @BeforeEach
     private void setUpPerTest() {
         try {
-         //   when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+      
 
             parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
             ticket = new Ticket();
             ticket.setInTime(new Date(System.currentTimeMillis() - (60*60*1000)));
             ticket.setParkingSpot(parkingSpot);
             ticket.setVehicleRegNumber("ABCDEF");
-       //     when(ticketDAO.getTicket(anyString())).thenReturn(ticket);
-       //     when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);
-
-        //    when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);
-
+ 
             parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         } catch (Exception e) {
             e.printStackTrace();
